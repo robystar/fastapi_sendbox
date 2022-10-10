33f9ceb8a4f281aecd55b43cb55d18c3fd29f946
Roberto Starnini <roberto.starnini@gmail.com>
2022-10-10 18:38:41 +0200

    tolto heroes

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import user, attivita, impresa, anagrafe, comune, ateco, hero, team, power, weapon, login, role, group, cache
+from app.api.v1.endpoints import user, attivita, impresa, anagrafe, comune, ateco,login, role, group, cache
 
 api_router = APIRouter()
 api_router.include_router(login.router, prefix="/login", tags=["login"])
@@ -15,8 +15,4 @@ api_router.include_router(comune.router, prefix="/comuni", tags=["Comuni"])
 api_router.include_router(role.router, prefix="/role", tags=["role"])
 api_router.include_router(user.router, prefix="/user", tags=["user"])
 api_router.include_router(group.router, prefix="/group", tags=["group"])
-api_router.include_router(team.router, prefix="/team", tags=["team"])
-api_router.include_router(hero.router, prefix="/hero", tags=["hero"])
-api_router.include_router(power.router, prefix="/power", tags=["power"])
-api_router.include_router(weapon.router, prefix="/weapon", tags=["weapon"])
 api_router.include_router(cache.router, prefix="/cache", tags=["cache"])
